213d51544394570705336bbca308f2e8593d9ad4
hrttn <hrettien@gmail.com>
2018-05-08 14:39:51 +0200

    Above folder created

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,12 +2,24 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('assets/styles/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'))
 });
 
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./src/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dest'));
+});
+
 //Watch task
 gulp.task('default', function() {
-    gulp.watch('sass/**/*.scss', ['styles']);
+    gulp.watch('assets/styles/**/*.scss', ['styles']);
 });
