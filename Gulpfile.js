e99ca6d77e68b73c854bf814a9995560b5be646d
hrttn <hrettien@gmail.com>
2018-05-10 09:04:53 +0200

    added autoprefixer

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
 
 gulp.task('styles', function() {
     gulp.src('assets/styles/**/*.scss')
@@ -7,19 +9,17 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('./css/'))
 });
 
-gulp.task('autoprefixer', function () {
-    var postcss      = require('gulp-postcss');
-    var sourcemaps   = require('gulp-sourcemaps');
-    var autoprefixer = require('autoprefixer');
-
-    return gulp.src('./src/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([ autoprefixer() ]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dest'));
-});
+gulp.task('autoprefixer', () =>
+	gulp.src('./css/*.css')
+		.pipe(autoprefixer({
+			browsers: ['last 5 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./css/'))
+);
 
 //Watch task
 gulp.task('default', function() {
     gulp.watch('assets/styles/**/*.scss', ['styles']);
+    gulp.watch('./css/*.css', ['autoprefixer']);
 });
