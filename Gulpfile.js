73bf4de98e8420d82c591c9a67ba8220dde61409
hrttn <hrettien@gmail.com>
2018-05-14 10:47:25 +0300

    Fixed SEO page

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const a11y = require('gulp-a11y');
+const concat = require('gulp-concat');
+const minifyCSS = require('gulp-minify-css');
+const rename = require('gulp-rename');
 
 
 gulp.task('styles', function() {
@@ -10,15 +13,14 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('./css/'))
 });
 
-gulp.task('autoprefixer', () =>
-	gulp.src('./css/*.css')
-		.pipe(autoprefixer({
-			browsers: ['last 5 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('./css/'))
-);
-
+gulp.task('css', function(){
+    gulp.src('assets/styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(minifyCSS())
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('./css/'))
+});
  
 gulp.task('accessibility', function () {
   return gulp.src('./**/*.html')
@@ -28,6 +30,5 @@ gulp.task('accessibility', function () {
 
 //Watch task
 gulp.task('default', function() {
-    gulp.watch('assets/styles/**/*.scss', ['styles']);
-    gulp.watch('./css/*.css', ['autoprefixer']);
+    gulp.watch('assets/styles/**/*.scss', ['css']);
 });
